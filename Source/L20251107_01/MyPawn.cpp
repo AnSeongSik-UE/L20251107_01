9209abc690ece25fe53fa848f8e1bdfd3b4fb4a9
AnSeongSik-unreal <ahnbs135@naver.com>
2025-11-07 17:01:15 +0900

    Mesh입히기, 프로펠러회전, 기체전진
    Mesh입히기, 프로펠러회전, 기체전진

--- Source/L20251107_01/MyPawn.cpp
+++ Source/L20251107_01/MyPawn.cpp
@@ -21,7 +21,12 @@ AMyPawn::AMyPawn()
 
 	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
 	Body->SetupAttachment(Box);
-	//메시 로딩
+	//개발 할 때는 안함, 이건 연습임
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Body(TEXT("/Script/Engine.StaticMesh'/Game/Meshes/SM_P38_Body.SM_P38_Body'"));
+	if (SM_Body.Succeeded())
+	{
+		Body->SetStaticMesh(SM_Body.Object);
+	}
 
 	Left = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Left"));
 	Left->SetupAttachment(Body);
@@ -30,6 +35,13 @@ AMyPawn::AMyPawn()
 	Right = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Right"));
 	Right->SetupAttachment(Body);
 	Right->SetRelativeLocation(FVector(37.f, 21.f, 1.f));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_Propeller(TEXT("/Script/Engine.StaticMesh'/Game/Meshes/SM_P38_Propeller.SM_P38_Propeller'"));
+	if (SM_Propeller.Succeeded())
+	{
+		Left->SetStaticMesh(SM_Propeller.Object);
+		Right->SetStaticMesh(SM_Propeller.Object);
+	}
 
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(RootComponent);
@@ -42,9 +54,10 @@ AMyPawn::AMyPawn()
 
 	Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
 	Arrow->SetupAttachment(RootComponent);
-	Arrow->SetRelativeLocation(FVector(50.f, 0, 0));
+	Arrow->SetRelativeLocation(FVector(70.f, 0, 0));
 
-	Movement;
+	Movement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement"));
+	Movement->MaxSpeed = 2000.f;
 }
 
 // Called when the game starts or when spawned
@@ -59,6 +72,10 @@ void AMyPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	AddMovementInput(GetActorForwardVector());
+	
+	Left->AddLocalRotation(FRotator(0, 0, 3600.f * DeltaTime));
+	Right->AddLocalRotation(FRotator(0, 0, 3600.f * DeltaTime));
 }
 
 // Called to bind functionality to input
